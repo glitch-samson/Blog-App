51947cba697bd0dc98b0457bc47323ce43653614
glitch-samson <glitchsamson375@gmail.com>
2025-04-24 02:31:35 +0100

    logo commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Search from "./Search";
 import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa6";
 import BlogsPage from "../pages/BlogsPage";
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -25,15 +26,14 @@ const Navbar = () => {
   }, []);
 
   return (
-    <header className={`flex items-center justify-between py-4 border-b z-[10001] transition-all duration-300 ease-in ${
-      isScrolled ? "fixed bg-white shadow-md md:px-32 top-0 left-0 right-0" : ""
-    }`}>
-      <a href="/" className="px-2 lg:px-0 uppercase font-bold text-purple-800">
-        The Blog-Spot
+    <header className={`flex items-center justify-between py-4 border-b z-[10001] transition-all duration-300 ease-in ${isScrolled ? "fixed bg-white shadow-md md:px-32 top-0 left-0 right-0" : ""
+      }`}>
+      <a href="/" className="px-2 lg:px-0">
+        <img src={logo} alt="The Blog-Spot Logo" width={120} height={40} />
       </a>
-      
+
       <div className="sm:block hidden">
-      <Search />
+        <Search />
       </div>
 
       <ul className="md:inline-flex items-center hidden">
@@ -77,15 +77,15 @@ const Navbar = () => {
             href="#"
             className="text-gray-500 font-semibold hover:text-purple-500"
           >
-           
+
             Login
           </a>
         </li>
-       
+
       </ul>
 
       <ul className="sm:hidden">
-      <li className="px-2 md:px-4">
+        <li className="px-2 md:px-4">
           <Link
             to="/"
             className="text-purple-600 font-semibold hover:text-purple-500"
